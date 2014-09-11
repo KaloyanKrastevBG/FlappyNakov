f90f176e23002cd26215e880e9868aaf27fde050
Petar Petkov <petar.b.petkov@gmail.com>
2014-09-11 17:16:28 -0500

    configuration added

--- src/org/softuni/Application.java
+++ src/org/softuni/Application.java
@@ -5,6 +5,10 @@ import javax.swing.JFrame;
 import org.softuni.Board;
 
 public class Application extends JFrame{
+	
+	//basic config
+	private int screenHeight = 1000;
+	private int screenWidth = 600;
 
 	public Application() {
 		//init method called in the constructor
@@ -13,7 +17,7 @@ public class Application extends JFrame{
 
 	private void initUI() {
 		add(new Board());
-		setSize(600,1000);
+		setSize(this.screenWidth,this.screenHeight);
 		setResizable(false);
 		setTitle("Flappy Nakov by Dark Blue Team");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
