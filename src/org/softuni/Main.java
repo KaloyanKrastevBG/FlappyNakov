8a57bc1981c370bbbc1a1af1bfca8fd072436764
petar <petar@petar-dev>
2014-09-14 19:43:03 +0300

    new gravity algorithm added

--- src/org/softuni/Main.java
+++ src/org/softuni/Main.java
@@ -14,12 +14,14 @@ public class Main {
 	public static final int GAME_SPEED = 15; // smaller - faster
 	public static final int PLAYER_DECCELERATION = 1; // the jumping const
 	public static final int GRAVITY_ACCELERATION = 1;
+	public static final int VERTICAL_SPEED = 10;
+	public static final int GRAVITY = 1;
 	public static final int MAX_PIPE_HEIGHT = 300;
 	public static final int MIN_PIPE_HEIGHT = 10;
 	public static final int PIPE_WIDTH = 29;
 	public static final int HAT_HEIGHT = 50;
 	public static final int HAT_WIDTH = 53;
-	public static final int PIPE_SPACING = 150;
+	public static final int PIPE_SPACING = 250;
 	public static final int PIPE_SPEED = 2;
 	public static final int PIPE_DISTANCE = 235;
 	public static final int GREEN_LINE_Y = Main.GAME_FLOOR + Player.getPlayerImg(
--- src/org/softuni/Player.java
+++ src/org/softuni/Player.java
@@ -4,33 +4,27 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
-
 import javax.swing.ImageIcon;
+
+
 
 public class Player extends Entity {
 
+	private boolean isJump = false;
+	private int speed = 0;
 	int velY = 0;
-
 	public Player(int x, int y) {
 		super(x, y);
 	}
 
 	// Player moving
 	public void update() {
-		if (velY <= 0) {
-			y += 10 * velY;
-			velY += 0 * Main.PLAYER_DECCELERATION;
+		if(isJump){
+			speed = -Main.VERTICAL_SPEED;
+			isJump = false;
 		}
-		if (y <= Main.SCREEN_HEIGHT) {
-			if (velY >= 0 && velY < 10) {
-				velY += Main.GRAVITY_ACCELERATION;
-			} else {
-				velY = 10;
-			}
-
-			y += velY;
-		}
-
+		speed += Main.GRAVITY;
+		y += speed;
 		if (y >= Main.GAME_FLOOR) {
 			y = Main.GAME_FLOOR;
 		}
@@ -59,7 +53,7 @@ public class Player extends Entity {
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
 		if (key == KeyEvent.VK_SPACE) {
-			velY = -10;
+			isJump = true;
 		}
 		if (key == KeyEvent.VK_ESCAPE) {
 			System.exit(0);
